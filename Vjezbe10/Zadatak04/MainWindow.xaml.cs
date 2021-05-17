b8579960437c6fe0053c0974414bdf4ed0e4baab
Luka Cabraja <luka@cabraja.eu>
2021-05-17 13:23:10 +0200

    vjezbe 10 zadatak 04 done

--- Vjezbe10/Zadatak04/MainWindow.xaml.cs
+++ Vjezbe10/Zadatak04/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Zadatak04
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }

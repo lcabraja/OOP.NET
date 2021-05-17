a1546893d9ba55652c5b8e0f77a145e5ca2e39e5
Luka Cabraja <luka@cabraja.eu>
2021-05-17 13:56:00 +0200

    vjezbe 10 zadatak 05 done

---
+++ Vjezbe10/Zadatak05/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadatak05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btPrint_Click(object sender, RoutedEventArgs e)
+        {
+            lbOutput.Content = tbInput.Text;
+            tbInput.Text = string.Empty;
+            tbInput.Focus();
+        }
+    }
+}
